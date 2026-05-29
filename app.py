bc3cdd048f58c8998061547065b66e43749ecae8
saisujith1994 <sai.s.ratham.setty@gmail.com>
2026-05-29 14:51:57 -0400

    This is the first commit

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, jsonify, request,render_template
+
+app=Flask(__name__) #WSGI (Web server gateway interface) application
+
+@app.route("/")
+def home():
+    return render_template("netflix_home.html")  # it will render the "home.html" template from templates folder when the user visits the root URL ("/") of the application.
+
+@app.route("/Techstack")
+def tech_stack():
+    return render_template("tech_stack.html")
+
+if __name__=="__main__":
+    app.run(debug=True)
